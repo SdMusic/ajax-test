481cbffa974bb729e26e32e833fd27c5d4b2dfa1
SdMusic <peter_freemantle@hotmail.com>
2020-10-28 11:38:21 +0000

    getting the data onto the page lesson

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
-var xhr = new XMLHttpRequest();
-var data;
+  
+const baseURL = "https://ci-swapi.herokuapp.com/api/";
 
-xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
-xhr.send();
+function getData(type, cb) {
+    var xhr = new XMLHttpRequest();
 
-xhr.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        data = JSON.parse(this.responseText);
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            cb(JSON.parse(this.responseText));
+        }
     };
+
+    xhr.open("GET", baseURL + type + "/");
+    xhr.send();
 }
 
-setTimeout(function() {
-    console.log(data);
-}, 500);
+function writeToDocument(type) {
+    getData(type, function(data) {
+        document.getElementById("data").innerHTML = data;
+    });
+}
